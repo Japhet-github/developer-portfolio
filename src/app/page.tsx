fad6c02243a90044e0bbaec42b3f30823461af39
Japhet <vannjaphet@gmail.com>
2024-10-29 13:33:18 +0200

    Refactor page component to add copyright information in footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,7 +113,9 @@ export default function Portfolio() {
 
       {/* Footer */}
       <footer className="bg-muted py-4 mt-16">
-
+        <div className="container mx-auto px-4 text-center">
+          <p>&copy; 2024 Japhet All rights reserved.</p>
+        </div>
       </footer>
     </div>
   );
